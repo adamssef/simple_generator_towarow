4beecdca9aa83b352c817ecbbb6beae20b30596b
adamssef <adam33ef@gmail.com>
2023-05-17 21:02:54 +0200

    Add generator

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        generateGoodsTxt("Towary.txt", 110000);
     }
+
+    static void generateGoodsTxt(String fileName, int numberOfGoods) {
+
+
+        try {
+            FileWriter writer = new FileWriter(fileName);
+
+            Random randomIds = new Random();
+            Random randomWeights = new Random();
+            randomIds.ints(1,11000)
+                            .limit(numberOfGoods)
+                            .boxed()
+                            .collect(Collectors.toList());
+
+            //weight in grams
+            randomWeights.ints(1, 11000)
+                            .limit(numberOfGoods)
+                            .boxed()
+                            .collect(Collectors.toList());
+
+            for (int i = 0; i < numberOfGoods; i++) {
+                writer.write("#" + randomIds.nextInt(11000) + " " + randomWeights.nextInt(100000) + "\n");
+            }
+
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred while writing to the input file.");
+            e.printStackTrace();
+        }
+    }
+
+
 }
